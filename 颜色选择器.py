0f1a779cae7155ff665e3236f330804822f6d8fc
xclala <66871338+xclala@users.noreply.github.com>
2020-10-24 14:24:32 +0800

    Add files via upload

--- 颜色选择器.py
+++ 颜色选择器.py
@@ -1,15 +1,19 @@
-#pyinstaller -F -w 颜色选择器.py
 from tkinter import colorchooser
-from tkinter.messagebox import showinfo
+from tkinter.messagebox import showinfo,askokcancel
 from tkinter import *
 
 
 def cc():
-    showinfo(title="颜色",message=colorchooser.askcolor(title="颜色选择"))
+    showinfo(title="颜色", message=colorchooser.askcolor(title="颜色选择"))
 
 
 root = Tk()
 root.title("颜色选择器")
 root.geometry("230x30")
-button = Button(root, text="选择颜色", command=cc).pack()
+
+def on_closing():
+    if messagebox.askokcancel("退出", "你确定要退出吗？"):
+        root.destroy()
+Button(root, text="选择颜色", command=cc).pack()
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
